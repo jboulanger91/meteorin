b20af2699085447e5e85f35ae6053a5ea6840c6c
jboulanger91 <jonathan.boulanger.weill@gmail.com>
2024-03-25 15:43:47 +0100

    test

--- dfc_tracking/dfc_tracking_figures.py
+++ dfc_tracking/dfc_tracking_figures.py
@@ -1,2 +1,54 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 from extract_positions_convergence_speed import extract_positions_convergence_speed
+
+# Videos have been downsampled to reduce size 
+down = 2
+# Pixel size
+pix_size = 0.637
+
+# Path to the Excel file
+path_data = '/Users/jonathanboulanger-weill/Desktop/Projects/meteorin/dfc_tracking/dfc_tracking.xlsx'
+
+# Load individual sheets
+wt1 = pd.read_excel(path_data, sheet_name=0) 
+wt1_centered_positions, wt1_conv_ratio, wt1_mig_speed = extract_positions_convergence_speed(pd.read_excel(path_data, sheet_name=0) , 'WT 180124', pix_size * down, 'g')
+
+wt2 = pd.read_excel(path_data, sheet_name=1)
+
+wt3 = pd.read_excel(path_data, sheet_name=2)
+mut1 = pd.read_excel(path_data, sheet_name=3)
+mut2 = pd.read_excel(path_data, sheet_name=4)
+mut3 = pd.read_excel(path_data, sheet_name=5)
+
+
+    # Plot each trajectories
+    for cell_idx in range(num_cells):
+        plt.plot(centered_positions[cell_idx, :, 0], centered_positions[cell_idx, :, 1],
+                 linewidth=1.5, color=plot_color)
+    
+    plt.legend([title], loc='upper center')
+    plt.xlabel('X position (um)',fontsize=14)
+    plt.ylabel('Y position (um)',fontsize=14)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+
+    ax.set_box_aspect(1)
+    plt.axis([-200, 200, 0, 400])
+    plt.gca().invert_yaxis()
+    # Set aspect ratio to 'equal' for square plot window
+    plt.figure(figsize=(12, 12))
+    #plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
+
+# Show trajctories for each video 
+plt.subplot(2, 5, 1)
+wt1_centered_positions, wt1_conv_ratio, wt1_mig_speed = extract_positions_convergence_speed(wt1, 'WT 180124', pix_size * down, 'g')
+plt.subplot(2, 5, 2)
+wt1_centered_positions, wt1_conv_ratio, wt1_mig_speed = extract_positions_convergence_speed(wt2, 'WT 180124', pix_size * down, 'g')
+
+
 
--- dfc_tracking/extract_positions_convergence_speed.py
+++ dfc_tracking/extract_positions_convergence_speed.py
@@ -2,61 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def extract_positions_convergence_speed(rec, title, pix_size, plot_color):
+
     # Number of cells per recording
-    num_cells = rec.shape[1] // 3
-    num_times = rec.shape[0]
+    num_cells = rec.shape[1] // 3  # Assuming each cell has x, y, and z columns
 
-    traj = {}
-    cells_pos = np.zeros((num_cells, 2, num_times))
+    # Loop over cells and extract positions for each time point
+    positions_per_cell = []
+    for cell_idx in range(num_cells):
+        cell_positions = []
+        for time_point in range(rec.shape[0]-1):
+            x = rec.iloc[time_point+1, cell_idx * 3 + 1]*pix_size
+            y = rec.iloc[time_point+1, cell_idx * 3 + 2]*pix_size
+            z = rec.iloc[time_point+1, cell_idx * 3 + 3]*pix_size
+            cell_positions.append((x, y, z))
+        positions_per_cell.append(cell_positions)
 
-    # Extract individual cell 2d positions
-    for idx_cell in range(num_cells):
-        traj['cell_posx'] = rec[:, idx_cell * 3 - 2]
-        traj['cell_posy'] = rec[:, idx_cell * 3 - 1]
-
-        # Z position is not taken into account
-        cells_pos[idx_cell, 0, :] = traj['cell_posx'] * pix_size
-        cells_pos[idx_cell, 1, :] = traj['cell_posy'] * pix_size
-
+    # Centering the data: 
     # Starting point should be 0 in y and mean(x) in x, to center plots
-    temp_xval = np.zeros(num_cells)
-    temp_yval = np.zeros(num_cells)
-    for idx_cell in range(num_cells):
-        temp_xval[idx_cell] = cells_pos[idx_cell, 0, 0]
-        temp_yval[idx_cell] = cells_pos[idx_cell, 1, 0]
-    start_xval = np.mean(temp_xval)
-    start_yval = np.min(temp_yval)
+    # Calculate the mean x-coordinate of all cells at the first time point
+    mean_x_init = np.mean([positions[0][0] for positions in positions_per_cell])
+    min_y_init = np.min([positions[0][1] for positions in positions_per_cell])
 
-    # Subtract to cell position
-    cells_pos_corrected = np.zeros_like(cells_pos)
-    for idx_cell in range(num_cells):
-        cells_pos_corrected[idx_cell, 0, :] = cells_pos[idx_cell, 0, :] - np.mean(temp_xval)
-        cells_pos_corrected[idx_cell, 1, :] = cells_pos[idx_cell, 1, :] - np.min(temp_yval)
+    # Subtract the mean x-coordinate of all cells at the first time point from each x-coordinate for each cell at each time point
+    centered_positions_per_cell = []
+    for cell_positions in positions_per_cell:
+        centered_cell_positions = [(x - mean_x_init, y-min_y_init, z) for x, y, z in cell_positions]
+        centered_positions_per_cell.append(centered_cell_positions)
 
     # Compute convergence ratio
-    x_scattering_init = np.max(cells_pos_corrected[:, 0, 0]) - np.min(cells_pos_corrected[:, 0, 0])
-    x_scattering_final = np.max(cells_pos_corrected[:, 0, 12]) - np.min(cells_pos_corrected[:, 0, 12])
-    conv_ratio = x_scattering_init / x_scattering_final
+    width_init = np.max([positions[0][0] for positions in positions_per_cell])-np.min([positions[0][0] for positions in positions_per_cell])
+    width_final = np.max([positions[12][0] for positions in positions_per_cell])-np.min([positions[12][0] for positions in positions_per_cell])
+    conv_ratio = width_init / width_final
+    conv_ratio
 
-    # Compute migration speed
+    # Compute migration speed, 12 time points equals to 3 hours recording
     mig_speed = np.zeros(num_cells)
     for idx_cell in range(num_cells):
-        mig_speed[idx_cell] = (cells_pos_corrected[idx_cell, 1, 12] - cells_pos_corrected[idx_cell, 1, 0]) / 3
+        mig_speed[idx_cell] = (centered_positions_per_cell[idx_cell][12][1] - centered_positions_per_cell[idx_cell][0][1]) / 3
 
-    # Plot each trajectories
-    for cell_idx in range(num_cells):
-        plt.plot(cells_pos_corrected[cell_idx, 0, :], cells_pos_corrected[cell_idx, 1, :],
-                 linewidth=1.5, color=plot_color)
-    
-    plt.legend([title], loc='upper center')
-    plt.xlabel('X position (um)')
-    plt.ylabel('Y position (um)')
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
-    plt.gca().spines['linewidth'] = 2
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.gca().invert_yaxis()
-    plt.tight_layout()
-    plt.show()
+    # Convert into an np array 
+    centered_positions = np.array(centered_positions_per_cell)
 
-    return cells_pos_corrected, conv_ratio, mig_speed
+    return centered_positions, conv_ratio, mig_speed
